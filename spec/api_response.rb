467bb2f337a7e6dcd6b9d269a681c80fd3ef6acc
Rayko <rayko.drg@gmail.com>
2014-06-03 14:45:25 -0300

    Added pending message to api_response_spec

--- spec/api_response.rb
+++ spec/api_response.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 require 'test_classes/projects'
 
 describe "APIResponse" do
+  pending "No previos spec"
   it "should parse response" do
-
+    # Code me maybe?
   end
 end
 
