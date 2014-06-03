d4b5183251967bd64006a165ceb71a006e35f9ec
Rayko <rayko.drg@gmail.com>
2014-06-03 14:31:36 -0300

    Updated syntax of Rspec expectations on api_parameter_spec

--- spec/api_parameter_spec.rb
+++ spec/api_parameter_spec.rb
@@ -9,16 +9,16 @@ describe GrapeDoc::APIParameter do
 
   describe "should respond to" do
     it "field" do
-      parameter.respond_to?(:field).should be_true
+      expect(parameter.respond_to?(:field)).to eq(true)
     end
     it "description" do
-      parameter.respond_to?(:description).should be_true
+      expect(parameter.respond_to?(:description)).to eq(true)
     end
     it "sample_value" do
-      parameter.respond_to?(:sample_value).should be_true
+      expect(parameter.respond_to?(:sample_value)).to eq(true)
     end
     it "field_type" do
-      parameter.respond_to?(:field_type).should be_true
+      expect(parameter.respond_to?(:field_type)).to eq(true)
     end
   end
 
@@ -27,15 +27,15 @@ describe GrapeDoc::APIParameter do
       document = parser.parse(Projects)[2]
       params = document.params
       params.each do |p|
-        p.kind_of?(APIParameter).should be_true
+        expect(p.kind_of?(APIParameter)).to eq(true)
       end
     end
     it "parse PUT project with parameters" do
       document = parser.parse(Projects)[2]
       param = document.params.first
-      param.field.should == "project_id"
-      param.description.should == "Project ID"
-      param.required.should be_true
+      expect(param.field).to eq("project_id")
+      expect(param.description).to eq("Project ID")
+      expect(param.required).to eq(true)
     end
   end
 end
