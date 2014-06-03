79bed50194879373ec9479bafa540339c53408a9
Rayko <rayko.drg@gmail.com>
2014-06-03 15:33:13 -0300

    Fixed doc_generator_spec

--- spec/doc_generator_spec.rb
+++ spec/doc_generator_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe GrapeDoc::DOCGenerator do
   it "resources" do
-    generator = DOCGenerator.new
+    generator = DOCGenerator.new 'test_classes/projects'
     expect(generator.resources.count).to eq(1)
   end
 end
