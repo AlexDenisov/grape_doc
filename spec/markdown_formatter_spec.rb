4a170c6c07a1ff1fb571057610c0391512160a13
Jack Forrest <jack@jrforrest.net>
2015-04-17 02:53:56 -0400

    Adds escaping for underscores in paths and param names

---
+++ spec/markdown_formatter_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe 'GrapeDoc::MarkdownFormatter' do
+  let(:formatter) { GrapeDoc::MarkdownFormatter.new }
+
+  let(:resource) do
+    instance_double('GrapeDoc::APIResource',
+      resource_name: 'MightyNeatResource',
+      documents: [document])
+  end
+
+  let(:param) do
+    instance_double('GrapeDoc::APIParameter',
+      field: 'radical_value',
+      field_type: 'String',
+      required: true,
+      description: 'This value is so very rad.  I bet you wish you were too.',
+      sample_value: 'raddish')
+  end
+
+  let(:document) do
+    instance_double('GrapeDoc::APIDocument',
+      params: [param],
+      http_method: 'POST',
+      path: '/a_twisty_path',
+      description: 'Nobody really knows what this does.'\
+        'Nobody but Zorp that is.')
+  end
+
+  subject(:markdown) { formatter.generate_resource_doc(resource) }
+
+  it 'escapes underscores in paths' do
+    expect(markdown).not_to match /\/a_twisty_path/
+    expect(markdown).to match /\/a\\_twisty\\_path/
+  end
+
+  it 'escapes underscores in param names' do
+    expect(markdown).not_to match /radical_value/
+    expect(markdown).to match /radical\\_value/
+  end
+end
